e5af15aaac88af20e522500c76c540954c4bbac7
Elpis-alpha <elpis409@gmail.com>
2022-02-18 13:46:57 +0100

    Proxy Removal

--- src/store/api.js
+++ src/store/api.js
@@ -1,21 +1,21 @@
 export const googleMapUrl = (lon, lat) => `https://google.com/maps?q=${lon},${lat}`
 
-export const autocompleteURL = (text) => `/api/weather/autocomplete?location=${text}`
+export const autocompleteURL = (text) => `https://elpis-weather-app.herokuapp.com/api/weather/autocomplete?location=${text}`
 
-export const weatherURL = (text) => `/api/weather/address?location=${text}`
+export const weatherURL = (text) => `https://elpis-weather-app.herokuapp.com/api/weather/address?location=${text}`
 
-export const userDuplicateURL = () => `/api/users/user`
+export const userDuplicateURL = () => `https://elpis-weather-app.herokuapp.com/api/users/user`
 
-export const createUserURL = () => `/api/users`
+export const createUserURL = () => `https://elpis-weather-app.herokuapp.com/api/users`
 
-export const loginUserURL = () => `/api/users/login`
+export const loginUserURL = () => `https://elpis-weather-app.herokuapp.com/api/users/login`
 
-export const logoutUserURL = () => `/api/users/logout`
+export const logoutUserURL = () => `https://elpis-weather-app.herokuapp.com/api/users/logout`
 
-export const getUserURL = () => `/api/users/user`
+export const getUserURL = () => `https://elpis-weather-app.herokuapp.com/api/users/user`
 
-export const createLocationURL = () => `/api/location`
+export const createLocationURL = () => `https://elpis-weather-app.herokuapp.com/api/location`
 
-export const getAllLocationURL = () => `/api/location`
+export const getAllLocationURL = () => `https://elpis-weather-app.herokuapp.com/api/location`
 
-export const deleteLocationURL = (id) => `/api/location/${id}`
+export const deleteLocationURL = (id) => `https://elpis-weather-app.herokuapp.com/api/location/${id}`
