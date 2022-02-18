841508c7f5281f09a7951bfe4e2a1c4068342374
Elpis-alpha <elpis409@gmail.com>
2022-02-18 13:19:02 +0100

    Creation

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
 
   app.use('/api', createProxyMiddleware({
 
-    target: 'http://localhost:5000',
+    target: 'https://elpis-weather-app.herokuapp.com',
 
     changeOrigin: true
 
